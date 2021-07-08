5c8167846936df3d290440084b8b629e6fcd3b13
Hoybaby <mbartek436@gmail.com>
2021-07-08 17:46:40 -0400

    just setting up my columns

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import './Footer.css'
 
 const Footer = () => {
     return(
-        <div>
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/* column 1 */}
+                    <div className="col">
+                        
+                    </div>
+                    {/* column 2 */}
+                    {/* column 3 */}
+                </div>
+            </div>
 
         </div>
     )
