0b05071e301a7e2a80a3fe62d9501db72d56821c
Hoybaby <mbartek436@gmail.com>
2021-07-08 12:30:08 -0400

    after a long time of trial and error I was finally able to call the information from the api onto the screen in this fashion. It is crude and rudimentary how i am calling the data but I am trying to the MVP first then build around it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react'
+import React, {useState} from 'react'
 import FetchApi from './components/FetchApi/FetchApi';
 
 
 class App extends React.Component {
+  state= {
+    // loading: true,
+    school: ''
+  }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     items: [],
+  //     isLoaded: false,
+  //   }
+  // }
+
+  async componentDidMount() {
+      const url = `https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&id=240444&api_key=${process.env.REACT_APP_CLIENT_APIKEY}`
+      const response = await fetch(url);
+
+      const data = await response.json();
+      console.log(data)
+      this.setState({school: data.results[0]})
+
+
+  }
+  
   render() {
-    return (
+  
+      return (
       <div className="App">
+        <h1>Education Analytics Project</h1>
+        {this.state.school.school? <p>{this.state.school.school.name}</p> : <p> No School yet</p>}
+        {this.state.school.school? <p>{this.state.school.school.school_url}</p> : <p> No School yet</p>}
+
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -22,11 +51,13 @@ class App extends React.Component {
             Learn React
           </a>
         </header> */}
-        <FetchApi/>
+        {/* <FetchApi/> */}
       </div>
       );
     }
   }
+    
+
   
 
 export default App;
--- src/components/FetchApi/FetchApi.jsx
+++ src/components/FetchApi/FetchApi.jsx
@@ -1,19 +1,75 @@
 import React from 'react'
 
-function FetchApi() {
+class FetchApi extends React.Component {
 
-    const apiGet =() => {
-        fetch(`https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&id=240444&api_key=${process.env.REACT_APP_CLIENT_APIKEY}`)
-        .then((response) => response.json())
-        .then((json => console.log(json)))
+    state = {
+        // loading: true,
+        school: null
     }
-    return (
-        <div>
-            <h1>Api Info</h1>
-            
-            <button onClick={apiGet}>Fetch Api</button>
-        </div>
-    )
+
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     items: [],
+    //     isLoaded: false,
+    //   }
+    // }
+
+    async componentDidMount() {
+        const url = `https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&id=240444&api_key=${process.env.REACT_APP_CLIENT_APIKEY}`
+        const response = await fetch(url);
+
+        const data = await response.json();
+        console.log(data)
+        this.setState({ school: data.results[0] })
+
+
+    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         error: null,
+    //         isLoaded: false,
+    //         items: []
+    //     };
+    // }
+    // componentDidMount() {
+    //     fetch(`https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&id=240444&api_key=${process.env.REACT_APP_CLIENT_APIKEY}`)
+    //         .then(res => res.json())
+    //         .then(
+    //             (result) => {
+    //                 this.setState({
+    //                     isLoaded: true,
+    //                     items: result[0]
+    //                 });
+    //             },
+    //             // Note: it's important to handle errors here
+    //             // instead of a catch() block so that we don't swallow
+    //             // exceptions from actual bugs in components.
+    //             (error) => {
+    //                 this.setState({
+    //                     isLoaded: true,
+    //                     error
+    //                 });
+    //             }
+    //         )
+    // }
+
+    render() {
+        return (
+            <ul>
+                <li>{this.state.school.school.name}</li>
+            </ul>
+        )
+    }
+
+    // const apiGet =() => {
+    //     fetch(`https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&id=240444&api_key=${process.env.REACT_APP_CLIENT_APIKEY}`)
+    //     .then((response) => response.json())
+    //     .then((json => console.log(json)))
+    // }
+
+
 }
 
 export default FetchApi;
