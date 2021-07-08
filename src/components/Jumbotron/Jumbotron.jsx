9ff52a77fde179ff2190b21b6a5e63f616908395
Hoybaby <mbartek436@gmail.com>
2021-07-08 16:13:46 -0400

    didnt link the css properly to the jumbotron and now i did to match the school colors

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './Jumbotron.css'
 
 const Jumbo = (props) => {
     return (
         <div>
-            <Jumbotron fluid className="jumbo-background">
+            <Jumbotron fluid className='jumbo-background'>
                 <Container fluid>
                     <h1 className="display-3">Univeristy of Wisconsin-Madison</h1>
-                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    <p className="lead">Below you will find some information and statistics about this specific University</p>
                 </Container>
             </Jumbotron>
         </div>
