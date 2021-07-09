8b8a568d1be256b42f00b32fb07b88ade862b7c9
Hoybaby <mbartek436@gmail.com>
2021-07-09 11:28:58 -0400

    ran into issue where background color wasn't changing but fixed it now

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -5,7 +5,7 @@ import './Jumbotron.css'
 const Jumbo = (props) => {
     return (
         <div>
-            <Jumbotron fluid className='jumbo-background'>
+            <Jumbotron fluid className='background-color'>
                 <Container fluid>
                     <h1 className="display-3">Univeristy of Wisconsin-Madison</h1>
                     <p className="lead">Below you will find some information and statistics about this specific University</p>
